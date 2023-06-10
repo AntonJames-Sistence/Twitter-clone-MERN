8b9b19e526b514af533d54a20c8c9335481ff681
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-06-10 18:25:52 -0400

    Add login route

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug');
+
+require('./models/User');
+require('./config/passport');
+const passport = require('passport');
+
+const csurf = require('csurf');
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
 
 const usersRouter = require('./routes/api/users'); // update the import file path
 const tweetsRouter = require('./routes/api/tweets'); // update the import file path
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -11,9 +21,43 @@ app.use(logger('dev')); // log request components (URL/method) to terminal
 app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
+app.use(passport.initialize());
+
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+};
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+);
 
 // Attach Express routers
 app.use('/api/users', usersRouter); // update the path
 app.use('/api/tweets', tweetsRouter); // update the path
+app.use('/api/csrf', csrfRouter);
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+});
 
 module.exports = app;
